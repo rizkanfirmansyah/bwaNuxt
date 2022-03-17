1a2c16d8c1bc1c16e25a4d80a535c9c97ebecc7a
rizkanfirmansyah <riezkanaprianda@gmail.com>
2022-03-17 11:15:17 +0700

    set base Url

--- nuxt.config.js
+++ nuxt.config.js
@@ -46,7 +46,7 @@ export default {
   // Axios module configuration: https://go.nuxtjs.dev/config-axios
   axios: {
     // Workaround to avoid enforcing hard-coded localhost:3000: https://github.com/nuxt-community/axios-module/issues/308
-    baseURL: '/',
+    baseURL: 'http://127.0.0.1:3000/',
   },
 
   // PWA module configuration: https://go.nuxtjs.dev/pwa
